b76d84274ba8ff9ff1daa120265703c835c6e0a1
Vinayak7989 <vinayakagarwal7989@gmail.com>
2022-09-18 16:24:39 +0530

    commit

---
+++ data.js
@@ -0,0 +1,77 @@
+const faqs = [
+  {
+    topic: "Program Eligibility and Suitability",
+    questions: [
+      {
+        title: "Who is eligible for the placement guarantee program?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "Can the program be completed along with college?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "I am experienced. Will this program be benificial for me?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "How many students will be there in my batch?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+    ],
+  },
+  {
+    topic: "Program Eligibility and Suitability",
+    questions: [
+      {
+        title: "Who is eligible for the placement guarantee program?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "Can the program be completed along with college?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "I am experienced. Will this program be benificial for me?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "How many students will be there in my batch?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+    ],
+  },
+  {
+    topic: "Program Eligibility and Suitability",
+    questions: [
+      {
+        title: "Who is eligible for the placement guarantee program?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "Can the program be completed along with college?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "I am experienced. Will this program be benificial for me?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+      {
+        title: "How many students will be there in my batch?",
+        description:
+          "We provide guaranteed placements for tech roles. This course is suitable for all engineering students graduating in 2023 or 2024, who are interested in tech placements.",
+      },
+    ],
+  },
+];
